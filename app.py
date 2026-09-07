8c4df8f02aa20f1ecf5370d26b7ea79a4cb87a32
cninokuni02 <cninokuni02@gmail.com>
2026-09-08 02:42:46 +0700

    feat: add app.py entrypoint for Render Web Service

---
+++ app.py
@@ -0,0 +1,7 @@
+﻿import uvicorn
+from backend.main import app
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 8989))
+    uvicorn.run("backend.main:app", host="0.0.0.0", port=port)
